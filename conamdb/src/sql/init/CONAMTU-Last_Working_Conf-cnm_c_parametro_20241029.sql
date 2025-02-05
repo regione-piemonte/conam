804de9e21673f24bf1ac3b7474aa2d6015969921
Riccardo LECCESE <riccardo.leccese@csi.it>
2025-02-05 09:31:55 +0100

    Aggiornamento versione di prodotto: 4.1.1

---
+++ conamdb/src/sql/init/CONAMTU-Last_Working_Conf-cnm_c_parametro_20241029.sql
@@ -0,0 +1,50 @@
+INSERT INTO conamtu.cnm_c_parametro (id_parametro,desc_parametro,valore_string,valore_number,valore_date,valore_boolean,inizio_validita,fine_validita) VALUES
+	 (3,'Oggetto pagamento rateizzazione','Rateizzazione di sanzioni amministrative',NULL,NULL,NULL,'2019-01-18',NULL),
+	 (4,'Codice Fiscale Ente Creditore','80087670016',NULL,NULL,NULL,'2019-01-18',NULL),
+	 (5,'Ente Creditore','Regione Piemonte',NULL,NULL,NULL,'2019-01-29',NULL),
+	 (6,'Cbill','ADRSY',NULL,NULL,NULL,'2019-01-29',NULL),
+	 (9,'Classificazione ','120312',NULL,NULL,NULL,'2019-02-08',NULL),
+	 (10,'Intestatario versamento postale','Tesoreria Regione Piemonte',NULL,NULL,NULL,'2019-02-08',NULL),
+	 (13,'Oggetto pagamento sollecito','Sollecito di pagamento ordinanza di ingiunzione',NULL,NULL,NULL,'2019-02-27',NULL),
+	 (38,'Info Ente web','la Regione Piemonte � Settore Politiche Fiscali - Contenzioso Amministrativo � Piazza Piemonte 1 - TORINO',NULL,NULL,NULL,'2023-06-05',NULL),
+	 (8,'Codice versamento Piemonte pay','QS01',NULL,NULL,NULL,'2019-01-18',NULL),
+	 (11,'Settore Ente','Direzione Risorse Finanziarie e Patrimonio Settore Politiche Fiscali e Contenzioso Amministrativo',NULL,NULL,NULL,'2019-02-08',NULL);
+INSERT INTO conamtu.cnm_c_parametro (id_parametro,desc_parametro,valore_string,valore_number,valore_date,valore_boolean,inizio_validita,fine_validita) VALUES
+	 (24,'Settore','Settore Politiche Fiscali e Contenzioso Amministrativo',NULL,NULL,NULL,'2019-03-01',NULL),
+	 (2,'Dirigente','Ai sensi dell’art. 3, c.2, del Decreto Legislativo 12 febbraio 1993, n. 39 la firma prevista per la validità della presente comunicazione, prodotta mediante sistemi informatici, è sostituita con l’indicazione a stampa del nominativo del soggetto responsabile.',NULL,NULL,NULL,'2019-01-18',NULL),
+	 (40,'TestoLiberoOrdinanzaArch','Ai sensi dell''art.',NULL,NULL,NULL,'2024-01-30',NULL),
+	 (17,'codice_prodotto','CONAM',NULL,NULL,NULL,'2020-07-01',NULL),
+	 (18,'codice_linea_cliente','RP-01',NULL,NULL,NULL,'2020-07-01',NULL),
+	 (20,'codice_unita_installazione','CONAMBL',NULL,NULL,NULL,'2020-07-01',NULL),
+	 (19,'codice_ambiente','TU',NULL,NULL,NULL,'2020-07-01',NULL),
+	 (7,'Autorizzazione','AUT. DB/SISB/GEN P1-PDF; 48237 DEL 13.11.2019',NULL,NULL,NULL,'2019-01-29',NULL),
+	 (41,'OggettoLettera','Lettera accompagnatoria ordinanza di archiviazione',NULL,NULL,NULL,'2024-02-07',NULL),
+	 (43,'ImportoSpeseProcessuali','Importo spese processuali',NULL,NULL,false,'2024-05-14',NULL);
+INSERT INTO conamtu.cnm_c_parametro (id_parametro,desc_parametro,valore_string,valore_number,valore_date,valore_boolean,inizio_validita,fine_validita) VALUES
+	 (44,'ImportoSpeseNotifica','Importo spese di notifica',NULL,NULL,false,'2024-05-14',NULL),
+	 (45,'IngPagamentoDet','INGIUNZIONE DI PAGAMENTO DD N. ',NULL,NULL,false,'2024-05-14',NULL),
+	 (42,'TipoComunicazioneSoap',NULL,NULL,NULL,false,'2024-04-08',NULL),
+	 (23,'Direzione','Direzione Risorse Finanziarie e Patrimonio',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (27,'Oggetto lettera annullamento','Determinazione di annullamento Prog. n. %s del %s in relazione all’ordinanza Prog. n. %s del %s nei confronti',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (28,'Organo accertatore','Organo accertatore;sedime + denominazione + n. civico;cap + comune + (PROV)',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (29,'Corpo lettera annullamento','Si trasmette in allegato copia conforme del provvedimento relativo all’oggetto.',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (30,'Saluti lettera annullamento','Cordiali saluti.',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (31,'Oggetto lettera sollecito rate parziale','Sollecito pagamento rate – determinazione di ingiunzione n. %s del %s',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (32,'Corpo (pt1) lettera sollecito rate parziale','Da una verifica contabile risulta che abbia provveduto al pagamento delle rate relative alla determinazione in oggetto sino alla data del %s, che la somma da versare ammonta a € %s e rimangono ancora da pagare n. %s rate.',NULL,NULL,NULL,'2021-03-01',NULL);
+INSERT INTO conamtu.cnm_c_parametro (id_parametro,desc_parametro,valore_string,valore_number,valore_date,valore_boolean,inizio_validita,fine_validita) VALUES
+	 (33,'Corpo (pt2) lettera sollecito rate parziale','Si invita pertanto al pagamento della somma sopra indicata entro il termine di dieci giorni dal ricevimento della presente. In caso di difetto si procederà all’avvio di esecuzione forzata per il recupero di quanto dovuto con ulteriore aggravio di spese e interessi a norma di legge.',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (34,'Oggetto lettera sollecito rate n.p.','Sollecito per rate non pervenute – Determinazione di ingiunzione n. %s del %s.',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (35,'Corpo (pt1) lettera sollecito rate n.p.','Su richiesta di rateizzazione pervenuta via pec in data _______ dal sig. __________, era stato concesso il pagamento rateale ai sensi dell’art. 26 della Legge 689/81.',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (36,'Corpo (pt2) lettera sollecito rate n.p.','A tutt’oggi non risulta pervenuto alcun versamento relativo alle istanze di rateizzazione in oggetto. Pertanto, se non provvederanno entro 10 giorni dal ricevimento della presente al versamento delle rate omesse, il settore scrivente dovrà ricorrere alle necessarie procedure di recupero dell’intero credito in un’unica soluzione, mediante riscossione coattiva.',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (21,'Data discriminante gestione pregresso',NULL,NULL,'2099-12-31',NULL,'2020-09-01',NULL),
+	 (22,'Data termine recupero',NULL,NULL,'2099-12-31',NULL,'2020-09-01',NULL),
+	 (1,'Numero conto Postale','001049335720',NULL,NULL,NULL,'2019-01-18',NULL),
+	 (14,'Oggetto pagamento ordinanza','Ordinanza di ingiunzione di pagamento 123456789101112131415161718192021',NULL,NULL,NULL,'2019-02-27',NULL),
+	 (37,'Oggetto pagamento sollecito rate','Sollecito di pagamento piano di rateizzazione (d.i. %s)',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (15,'Autore fisico ACTA','autore fisico ACTA',NULL,NULL,NULL,'2019-08-07',NULL);
+INSERT INTO conamtu.cnm_c_parametro (id_parametro,desc_parametro,valore_string,valore_number,valore_date,valore_boolean,inizio_validita,fine_validita) VALUES
+	 (25,'Dirigente del settore','Enrico Miniotti',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (39,'Info Ente testo','la Regione Piemonte � Settore Politiche Fiscali - Contenzioso Amministrativo � Piazza Piemonte 1 - TORINO',NULL,NULL,NULL,'2023-06-05',NULL),
+	 (26,'Sede Ente','Piazza Piemonte 1;10127 Torino (TO);Tel: 011 4321288;Email: politiche.fiscali@cert.regione.piemonte.it',NULL,NULL,NULL,'2021-03-01',NULL),
+	 (16,'mail settore tributi','politiche.fiscali@cert.regione.piemonte.it',NULL,NULL,NULL,'2020-02-01',NULL),
+	 (12,'Info Ente','Piazza Piemonte 1 Torino politiche.fiscali@cert.regione.piemonte.it ',NULL,NULL,NULL,'2019-02-08',NULL);
